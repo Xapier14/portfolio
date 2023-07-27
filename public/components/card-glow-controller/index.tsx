bdd14fd1a94ef8d307dcd05241573c81a977a721
Lance Crisang <lancecrisang14@gmail.com>
2023-07-27 12:27:31 +0800

    Fix pre-render

--- public/components/card-glow-controller/index.tsx
+++ public/components/card-glow-controller/index.tsx
@@ -1,18 +1,24 @@
 import { useEffect } from "preact/hooks";
 
 export function CardGlowController() {
-  const root = document.querySelector(":root") as HTMLElement;
-
-  root.style.setProperty("--mouse-x", `-1000px`);
-  root.style.setProperty("--mouse-y", `-1000px`);
+  const isPrerender = typeof window === "undefined";
+  var root = null;
+  if (!isPrerender) {
+    root = document.querySelector(":root") as HTMLElement;
+    root.style.setProperty("--mouse-x", `-1000px`);
+    root.style.setProperty("--mouse-y", `-1000px`);
+  }
 
   function onMouseMove(e: MouseEvent) {
     if (root) {
       root.style.setProperty("--mouse-x", `${e.clientX}px`);
       root.style.setProperty("--mouse-y", `${e.clientY}px`);
+    } else {
+      root = document.querySelector(":root") as HTMLElement;
     }
   }
   useEffect(() => {
+    if (isPrerender) return;
     // if mobile
     if (window.innerWidth <= 768) {
       return;
