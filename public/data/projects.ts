5326e2045b7b9ebe0bbcab847ecc4003834f37a4
Lance Crisang <lancecrisang14@gmail.com>
2023-08-10 10:22:49 +0800

    Move project data into seperate file

---
+++ public/data/projects.ts
@@ -0,0 +1,40 @@
+import { JSXInternal } from "preact/src/jsx";
+import projectsDataSource from "./projects.json";
+
+import {
+  CloudIcon,
+  CopyIconMini,
+  DataIcon,
+  DevIcon,
+  MobileIcon,
+  ServerIcon,
+  WebIcon,
+} from "../components/icons";
+
+interface Project {
+  name: string;
+  id: string;
+  type: "web" | "mobile" | "desktop" | "cloud";
+  feature: string;
+  description?: string;
+  demo?: string;
+  github?: string;
+}
+
+const projects = projectsDataSource as unknown as Project[];
+function getIcon(icon: string): () => JSXInternal.Element | null {
+  switch (icon) {
+    case "web":
+      return WebIcon;
+    case "mobile":
+      return MobileIcon;
+    case "desktop":
+      return DevIcon;
+    case "cloud":
+      return CloudIcon;
+    default:
+      return null;
+  }
+}
+
+export { projects, getIcon };
