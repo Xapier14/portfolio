3eecf66137e4f00f136f316e55487173034b4a25
Lance <lancecrisang14@gmail.com>
2023-09-05 11:14:35 +0800

    Fix navbar and remove landing section hack (#5)

--- public/components/navbar/index.tsx
+++ public/components/navbar/index.tsx
@@ -1,37 +1,76 @@
-import { useEffect } from "preact/hooks";
+import { useEffect, useState } from "preact/hooks";
 import style from "./style.module.scss";
 import "../common/card-style.scss";
 import { useLocation } from "preact-iso";
 
 export function NavBar() {
-  var autoHide = false;
+  var [autoHide, setAutoHide] = useState(false);
   const location = useLocation();
-  useEffect(() => {
-    let path = location.url;
-    autoHide = path == "/";
-    console.log(`NavBar Auto-Hide: ${autoHide}`);
+  var [path, setPath] = useState("");
+
+  const onScroll = () => {
     var navbar = document.getElementsByClassName(style["navbar"])[0];
-    if (navbar && !autoHide) {
-      navbar.classList.remove(style["hidden"]);
-      return;
-    }
-    // scroll event listener
-    window.addEventListener("scroll", () => {
-      if (navbar) {
-        let path = location.url;
-        autoHide = path == "/";
-        if (!autoHide) {
-          navbar.classList.remove(style["hidden"]);
-          return;
-        }
-        if (window.scrollY > 0) {
-          navbar.classList.remove(style["hidden"]);
-        } else {
-          navbar.classList.add(style["hidden"]);
-        }
+    if (navbar) {
+      const autoHideNow = path == "/";
+      setAutoHide(autoHideNow);
+      console.log(`autoHide from scroll is ${autoHideNow}`);
+      // console.log(`[scroll] path`);
+      // console.log(path);
+      if (!autoHideNow) {
+        navbar.classList.remove(style["hidden"]);
+        return;
       }
-    });
+      if (window.scrollY > 0) {
+        navbar.classList.remove(style["hidden"]);
+      } else {
+        console.log("hidden by scroll");
+        navbar.classList.add(style["hidden"]);
+      }
+    }
+  };
+
+  useEffect(() => {
+    console.log("locationHook trigger");
+    console.log(location);
+    setPath(location.path);
+    console.log(`path updated: ${location.path}`);
   }, [location]);
+
+  useEffect(() => {
+    console.log("pathHook trigger");
+    const autoHideNow = path == "/";
+    setAutoHide(autoHideNow);
+
+    window.removeEventListener("scroll", onScroll);
+    if (!autoHideNow) {
+      window.addEventListener("scroll", onScroll);
+    }
+
+    var navbar = document.getElementsByClassName(style["navbar"])[0];
+    if (navbar != null && !autoHideNow) {
+      console.log("unhiding navbar");
+      navbar.classList.remove(style["hidden"]);
+    }
+  }, [path]);
+
+  useEffect(() => {
+    // clean up code
+    window.removeEventListener("scroll", onScroll);
+    window.addEventListener("scroll", onScroll, { passive: true });
+    return () => {
+      var navbar = document.getElementsByClassName(style["navbar"])[0];
+      const autoHideNow = path == "/";
+      setAutoHide(autoHideNow);
+
+      window.removeEventListener("scroll", onScroll);
+
+      if (navbar != null && !autoHideNow) {
+        console.log("unhiding navbar");
+        navbar.classList.remove(style["hidden"]);
+      }
+    };
+  });
+
   return (
     <>
       <header
