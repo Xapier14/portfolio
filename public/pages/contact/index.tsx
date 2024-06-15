b6c7fb9881da8b8c2907ad8f37d73afbebd707df
Lance <lancecrisang14@gmail.com>
2024-06-15 20:17:45 +0800

    Add contact panel

--- public/pages/contact/index.tsx
+++ public/pages/contact/index.tsx
@@ -1,8 +1,21 @@
+import styles from "./index.module.scss";
+import ShowcaseCard from "../../components/showcase-card";
+
+const emailAddress = "lance@xapier.me";
+
 const Contact = () => {
   return (
     <>
-      <section class="contact">
-        <h1>Contact</h1>
+      <section class={styles.contact}>
+        <div class={styles.container}>
+          <ShowcaseCard name="Contact" class={styles["contact-card"]}>
+            <p>
+              Do you have a project in mind? Or do you just want to say hi?
+              <br/>
+              Feel free to leave a message below or send me an email at   <a href={`mailto:${emailAddress}`}>{emailAddress}</a>.
+            </p>
+          </ShowcaseCard>
+        </div>
       </section>
     </>
   );
